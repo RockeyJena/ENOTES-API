b2bec97a6622394388dd885ef3c7712933c72b8f
RockeyJena <rockeyjena.in1999@gmail.com>
2025-02-17 23:28:07 +0530

    category dto and active category code implementation

--- src/main/java/com/Enotes_Api_Service/Repository/CategoryRepository.java
+++ src/main/java/com/Enotes_Api_Service/Repository/CategoryRepository.java
@@ -1,7 +1,12 @@
 package com.Enotes_Api_Service.Repository;
 
+import com.Enotes_Api_Service.Dto.CategoryDto;
 import com.Enotes_Api_Service.Entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category,Integer> {
+
+    List<Category> findAllByIsActiveTrue();
 }
